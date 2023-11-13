ded41aa43168a40074913b3c760a048d698f3ab6
Schadde-Ronny <frevert71@gmail.com>
2023-11-13 18:10:42 +0100

    Erstellung eines random Generators im moment für Chuck Norris Witze (Kein Sichtbarer Fehler im Devtool) Inhalt wird nicht auf der Website angezeigt

--- hackerDataCenterApp/src/components/MemeJokeQuote.jsx
+++ hackerDataCenterApp/src/components/MemeJokeQuote.jsx
@@ -1,9 +1,26 @@
+import {useState, useEffect } from "react";
 
 const MemeJokeQuote = () => {
-  
+  const [content, setContent] = useState("");
+
+  useEffect(() => {
+    //Abrufen eines zufälligen Chuck Norris witz von einer API
+    const fetchRandomContent = async () => {
+      try {
+        const response = await fetch("https://api.chucknorris.io/jokes/random");
+        const data = await response.json();
+        setContent(data.content);
+      } catch (error) {
+        console.error("Fehler beim Abrufen von Inhalten:", error);
+      }
+    };
+
+    // Aufruf der API beim laden der Komponente
+    fetchRandomContent();
+  }, []);
   return (
     <>
-      
+      <p>{content}</p>
     </>
   );
 };
