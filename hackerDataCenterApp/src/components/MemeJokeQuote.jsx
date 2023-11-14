7e365dbb8b7f29d571a53975bd48d573a891088f
Schadde-Ronny <frevert71@gmail.com>
2023-11-14 13:50:20 +0100

    fehler behoben bezüglich button

--- hackerDataCenterApp/src/components/MemeJokeQuote.jsx
+++ hackerDataCenterApp/src/components/MemeJokeQuote.jsx
@@ -4,40 +4,32 @@ const MemeJokeQuote = () => {
   const [content, setContent] = useState("");
   const [isMeme, setIsMeme] = useState(false);
 
-  const fetchRandomContent = async (url) => {
-    try {
-      const response = await fetch(url);
-      const data = await response.json();
-
-      
-      const contentValue = isMeme ? data.data.memes[0].url : data.value;
-
-      setContent(contentValue);
-    } catch (error) {
-      console.error("Fehler beim Abrufen von Inhalten:", error);
-    }
-  };
-
-  const handleClick = () => {
-    setIsMeme(!isMeme); // Um zwischen Meme und Chuck Norris zu wechseln
-    const url = isMeme
-      ? "https://api.imgflip.com/get_memes"
-      : "https://api.chucknorris.io/jokes/random?category=dev";
-    fetchRandomContent(url);
-  };
-
   useEffect(() => {
-
-    const initialUrl = "https://api.chucknorris.io/jokes/random?category=dev";
-    fetchRandomContent(initialUrl);
-  }, []);
+    const url = isMeme
+    ? "https://api.imgflip.com/get_memes"
+    : "https://api.chucknorris.io/jokes/random?category=dev";
+    const fetchRandomContent = async (url) => {
+      try {
+        const response = await fetch(url);
+        const data = await response.json();
+  
+        
+        const contentValue = isMeme ? data.data.memes[0].url : data.value;
+  
+        setContent(contentValue);
+      } catch (error) {
+        console.error("Fehler beim Abrufen von Inhalten:", error);
+      }
+    };
+    fetchRandomContent(url); setContent("")
+  }, [isMeme]);
 
   return (
     <>
       <div>
-        {isMeme ? <img src={content} alt="Meme" /> : <p>{content}</p>}
+        {content && isMeme ? <img src={content} alt="Meme" /> : <p>{content}</p>}
       </div>
-      <button onClick={handleClick}>Wechseln</button>
+      <button onClick={() => setIsMeme(!isMeme)}>Wechseln</button>
     </>
   );
 };
