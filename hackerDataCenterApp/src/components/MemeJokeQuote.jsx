b86a709ef2bcd1e2e10cb7e0557bd17dd668c2d1
Schadde-Ronny <frevert71@gmail.com>
2023-11-14 12:29:11 +0100

    implementierung eines Memegenerator und ein Button mit der funktion random von der url auszusuchen

--- hackerDataCenterApp/src/components/MemeJokeQuote.jsx
+++ hackerDataCenterApp/src/components/MemeJokeQuote.jsx
@@ -1,29 +1,43 @@
-import {useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 
 const MemeJokeQuote = () => {
-  const [content, setContent] = useState([]);
+  const [content, setContent] = useState("");
+  const [isMeme, setIsMeme] = useState(false);
+
+  const fetchRandomContent = async (url) => {
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+
+      
+      const contentValue = isMeme ? data.data.memes[0].url : data.value;
+
+      setContent(contentValue);
+    } catch (error) {
+      console.error("Fehler beim Abrufen von Inhalten:", error);
+    }
+  };
+
+  const handleClick = () => {
+    setIsMeme(!isMeme); // Um zwischen Meme und Chuck Norris zu wechseln
+    const url = isMeme
+      ? "https://api.imgflip.com/get_memes"
+      : "https://api.chucknorris.io/jokes/random?category=dev";
+    fetchRandomContent(url);
+  };
 
   useEffect(() => {
-    //Abrufen eines zufälligen Chuck Norris witz von einer API
-    const fetchRandomContent = async () => {
-      try {
-        const response = await fetch("https://api.chucknorris.io/jokes/random?category=dev");
-        const data = await response.json();
-        setContent(data);
-      } catch (error) {
-        console.error("Fehler beim Abrufen von Inhalten:", error);
-      }
-    };
 
-    // Aufruf der API beim laden der Komponente
-    fetchRandomContent();
+    const initialUrl = "https://api.chucknorris.io/jokes/random?category=dev";
+    fetchRandomContent(initialUrl);
   }, []);
+
   return (
     <>
-    <div>
-    <p>{content.value}</p>
-    </div>
-      
+      <div>
+        {isMeme ? <img src={content} alt="Meme" /> : <p>{content}</p>}
+      </div>
+      <button onClick={handleClick}>Wechseln</button>
     </>
   );
 };
