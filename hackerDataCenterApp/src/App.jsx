fbfd8c0a7ead799591e6807a065abd3c21f7495c
Friedemann Denzer <friedemann_denzer@posteo.de>
2023-11-15 10:51:02 +0100

    priceTicker fast fertig, VOR der CryptoChart Implementierung

--- hackerDataCenterApp/src/App.jsx
+++ hackerDataCenterApp/src/App.jsx
@@ -1,28 +1,36 @@
 import "./App.css";
 import MemeJokeQuote from "./components/MemeJokeQuote.jsx";
+import PriceTicker from "./components/PriceTicker.jsx";
 import Wetter from "./components/Wetter.jsx";
 
 function App() {
   // Komponenten können im Ordner 'components' angelegt und hier im jeweiligen div eingefügt werden
   return (
     <>
-      <div className="container">
-        <div className="news"></div>
-        <div className="wetter">
-          <Wetter />
-        </div>
-        <div className="Börse"></div>
-        <div className="quoteJokeFact">
-          <MemeJokeQuote />
-        </div>
-        <div className="orderPizza"></div>
-        <div className="chat"></div>
-        <div className="deepL"></div>
-        <div className="sthCoolLikeNasa"></div>
-        <div className="darknetMarket"></div>
-        <div className="stockMarket"></div>
-        <div className="tinder"></div>
-      </div>
+<div className="container">
+  <div className="newsTicker">
+    <PriceTicker />
+  </div>
+  <div className="sthCoolLikeNasa">Nasa Shit</div>
+  <div className="chat">Chat</div>
+  <div className="quoteJokeFact">
+    <MemeJokeQuote />
+  </div>
+  <div className="orderPizza">orderPizza</div>
+  <div className="darknetMarket">darknet Market</div>
+  <div className="stockMarket">
+    stock Market
+    
+
+    
+    </div>
+  <div className="deepL">deep L </div>
+  <div className="wetter">
+    <Wetter />
+  </div>
+  <div className="news">News</div>
+  <div className="kontaktBörse">Tinmder</div>
+</div>
     </>
   );
 }
