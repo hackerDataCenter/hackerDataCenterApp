695fd47b011c5bf97d9d79af6eba5bd9a5cf1afa
Schadde-Ronny <frevert71@gmail.com>
2023-11-15 19:40:30 +0100

    Seperatieren des Modulafenster für den button (buttons müssen noch ausgetauscht werden)

--- hackerDataCenterApp/src/App.jsx
+++ hackerDataCenterApp/src/App.jsx
@@ -1,17 +1,20 @@
 import "./App.css";
 import MemeJokeQuote from "./components/MemeJokeQuote.jsx";
 import Wetter from "./components/Wetter.jsx";
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import { ModalContext } from "./global/ModalProvider.jsx";
 import { Modal } from "./reusable/Modal.jsx";
 import { ModalBG } from "./reusable/modalbg.jsx";
+import { EssenBestellen } from "./components/EssenBestellen.jsx";
 
 function App() {
   // Komponenten können im Ordner 'components' angelegt und hier im jeweiligen div eingefügt werden
   const {open, setOpen, setLoad} = useContext(ModalContext)
-  const modalHaendler = (e, loader) => {
+  const [fetchIdent, setFetchIdent] = useState("");
+  const modalHaendler = (e, string) => {
    e.preventDefault();
-   setLoad(loader)
+
+   setFetchIdent(string)
    setOpen(!open)
 }
   useEffect(() => {
@@ -20,24 +23,26 @@ function App() {
 
   return (
     <>
-    {open && <div className="modalContainer"> <Modal/> <ModalBG/></div>}
+    {open && <div className="modalContainer"> <Modal fetchIdent={fetchIdent} /> <ModalBG/> </div>}
       
       <div className="container">
-        <div className="news"><button className="placeHolder-Button" onClick={(e) => modalHaendler(e, "test")}></button></div>
+        <div className="news"><button className="placeHolder-Button" onClick={(e) => modalHaendler(e, "news")}></button></div>
         <div className="wetter">
-          <Wetter /><button className="placeHolder-Button" onClick={(e) => modalHaendler(e)}></button>
+          <Wetter /><button className="placeHolder-Button" onClick={(e) => modalHaendler(e, "weather")}></button>
         </div>
-        <div className="Börse"><button className="placeHolder-Button" onClick={(e) => modalHaendler(e)}></button></div>
+        <div className="boerse"><button className="placeHolder-Button" onClick={(e) => modalHaendler(e, "hallo")}></button></div>
         <div className="quoteJokeFact">
           <MemeJokeQuote /> 
         </div>
-        <div className="orderPizza"><button className="placeHolder-Button" onClick={(e) => modalHaendler(e)}></button></div>
-        <div className="chat"><button className="placeHolder-Button" onClick={(e) => modalHaendler(e)}></button ></div>
-        <div className="deepL"><button className="placeHolder-Button" onClick={(e) => modalHaendler(e)}></button ></div>
-        <div className="sthCoolLikeNasa"><button className="placeHolder-Button" onClick={(e) => modalHaendler(e)}></button></div>
-        <div className="darknetMarket"><button className="placeHolder-Button" onClick={(e) => modalHaendler(e)}></button></div>
-        <div className="stockMarket"><button className="placeHolder-Button" onClick={(e) => modalHaendler(e)}></button></div>
-        <div className="tinder"><button className="placeHolder-Button" onClick={(e) => modalHaendler(e)}></button></div>
+        <div className="orderPizza"><button className="placeHolder-Button" onClick={(e) => modalHaendler(e, "essen")}></button>
+        <EssenBestellen/>
+        </div>
+        <div className="chat"><button className="placeHolder-Button" onClick={(e) => modalHaendler(e, "chat")}></button></div>
+        <div className="deepL"><button className="placeHolder-Button" onClick={(e) => modalHaendler(e, "deepl")}></button ></div>
+        <div className="sthCoolLikeNasa"><button className="placeHolder-Button" onClick={(e) => modalHaendler(e, "sthcoolLikeNasa")}></button></div>
+        <div className="darknetMarket"><button className="placeHolder-Button" onClick={(e) => modalHaendler(e, "darknetMarket")}></button></div>
+        <div className="stockMarket"><button className="placeHolder-Button" onClick={(e) => modalHaendler(e, "stockMarket")}></button></div>
+        {/* <div className="tinder"><button className="placeHolder-Button" onClick={(e) => modalHaendler(e, "tinder")}></button></div> */}
       </div>
     </>
   );
--- hackerDataCenterApp/src/reusable/Modal.jsx
+++ hackerDataCenterApp/src/reusable/Modal.jsx
@@ -1,11 +1,42 @@
 import { useContext } from "react";
 import { ModalContext } from "../global/ModalProvider.jsx";
+import MemeJokeQuote from "../components/MemeJokeQuote.jsx";
+import { EssenBestellen } from "../components/EssenBestellen.jsx";
 // Geladen werden nur Components kein Hardcode
 // loader=string swtichcase nach string
-export const Modal = () => {
-    const {setOpen, loader} = useContext(ModalContext)
+export const Modal = ({fetchIdent}) => {
+    const {setOpen, } = useContext(ModalContext)
+    let render = ""
+    if (fetchIdent === "weather"){
+      render = (<h1>Platzhalter</h1>) 
+    }else if (fetchIdent === "essen"){
+        render = (<EssenBestellen/>)
+    }else if (fetchIdent === "news"){
+        render = (<h2>Platzhalter</h2>)
+    }else if (fetchIdent === "hallo"){
+            render = (<h2>Platzhalter</h2>)
+    }else if (fetchIdent === "chat"){
+        render = (<h2>Platzhalter</h2>)
+    }else if (fetchIdent === "chat"){
+        render = (<h2>Platzhalter</h2>)
+    }else if (fetchIdent === "deepl"){
+        render = (<h2>Platzhalter</h2>)
+    }else if (fetchIdent === "sthcoolLikeNasa"){
+        render = (<h2>Platzhalter</h2>)
+    }else if (fetchIdent === "darknetMarket"){
+        render = (<h2>Platzhalter</h2>)
+    }else if (fetchIdent === "stockMarket"){
+        render = (<h2>Platzhalter</h2>)
+    }
     return(
-        <div className="modal">Test<i className="fa-solid fa-circle-xmark close" onClick={()=> setOpen(!open)}></i></div>
+        <div className="modal">  <i className="fa-solid fa-circle-xmark close" onClick={()=> setOpen(!open)}></i>
+        {render}
+        </div>
+
+        
+         
+        
+        
         
     )
 }
