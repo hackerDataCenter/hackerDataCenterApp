d95434dacdb521d464f3b1f091742aa02a088fdc
Schadde-Ronny <frevert71@gmail.com>
2023-11-16 09:29:22 +0100

    styling

--- hackerDataCenterApp/src/App.jsx
+++ hackerDataCenterApp/src/App.jsx
@@ -6,7 +6,7 @@ import { ModalContext } from "./global/ModalProvider.jsx";
 import { Modal } from "./reusable/Modal.jsx";
 import { ModalBG } from "./reusable/modalbg.jsx";
 import { EssenBestellen } from "./components/EssenBestellen.jsx";
-
+import { News } from "./components/News.jsx";
 function App() {
   // Komponenten können im Ordner 'components' angelegt und hier im jeweiligen div eingefügt werden
   const {open, setOpen, setLoad} = useContext(ModalContext)
@@ -26,22 +26,22 @@ function App() {
     {open && <div className="modalContainer"> <Modal fetchIdent={fetchIdent} /> <ModalBG/> </div>}
       
       <div className="container">
-        <div className="news"><button className="placeHolder-Button" onClick={(e) => modalHaendler(e, "news")}></button></div>
-        <div className="wetter">
-          <Wetter /><button className="placeHolder-Button" onClick={(e) => modalHaendler(e, "weather")}></button>
+        <div className="news"onClick={(e) => modalHaendler(e, "news")}> <News/> </div>
+        <div className="wetter"onClick={(e) => modalHaendler(e, "weather")}>
+          <Wetter/>
         </div>
-        <div className="boerse"><button className="placeHolder-Button" onClick={(e) => modalHaendler(e, "hallo")}></button></div>
+        <div className="boerse"onClick={(e) => modalHaendler(e, "hallo")}></div>
         <div className="quoteJokeFact">
           <MemeJokeQuote /> 
         </div>
-        <div className="orderPizza"><button className="placeHolder-Button" onClick={(e) => modalHaendler(e, "essen")}></button>
+        <div className="orderPizza"onClick={(e) => modalHaendler(e, "essen")}>
         <EssenBestellen/>
         </div>
-        <div className="chat"><button className="placeHolder-Button" onClick={(e) => modalHaendler(e, "chat")}></button></div>
-        <div className="deepL"><button className="placeHolder-Button" onClick={(e) => modalHaendler(e, "deepl")}></button ></div>
-        <div className="sthCoolLikeNasa"><button className="placeHolder-Button" onClick={(e) => modalHaendler(e, "sthcoolLikeNasa")}></button></div>
-        <div className="darknetMarket"><button className="placeHolder-Button" onClick={(e) => modalHaendler(e, "darknetMarket")}></button></div>
-        <div className="stockMarket"><button className="placeHolder-Button" onClick={(e) => modalHaendler(e, "stockMarket")}></button></div>
+        <div className="chat" onClick={(e) => modalHaendler(e, "chat")}></div>
+        <div className="deepL"onClick={(e) => modalHaendler(e, "deepl")}></div>
+        <div className="sthCoolLikeNasa"onClick={(e) => modalHaendler(e, "sthcoolLikeNasa")}></div>
+        <div className="darknetMarket"onClick={(e) => modalHaendler(e, "darknetMarket")}></div>
+        <div className="stockMarket"onClick={(e) => modalHaendler(e, "stockMarket")}></div>
         {/* <div className="tinder"><button className="placeHolder-Button" onClick={(e) => modalHaendler(e, "tinder")}></button></div> */}
       </div>
     </>
--- hackerDataCenterApp/src/reusable/Modal.jsx
+++ hackerDataCenterApp/src/reusable/Modal.jsx
@@ -1,18 +1,19 @@
 import { useContext } from "react";
 import { ModalContext } from "../global/ModalProvider.jsx";
-import MemeJokeQuote from "../components/MemeJokeQuote.jsx";
 import { EssenBestellen } from "../components/EssenBestellen.jsx";
+import Wetter from "../components/Wetter.jsx";
+import { News } from "../components/News.jsx";
 // Geladen werden nur Components kein Hardcode
 // loader=string swtichcase nach string
 export const Modal = ({fetchIdent}) => {
     const {setOpen, } = useContext(ModalContext)
     let render = ""
     if (fetchIdent === "weather"){
-      render = (<h1>Platzhalter</h1>) 
+      render = (<Wetter/>) 
     }else if (fetchIdent === "essen"){
         render = (<EssenBestellen/>)
     }else if (fetchIdent === "news"){
-        render = (<h2>Platzhalter</h2>)
+        render = (<News/>)
     }else if (fetchIdent === "hallo"){
             render = (<h2>Platzhalter</h2>)
     }else if (fetchIdent === "chat"){
